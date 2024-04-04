cf76f1cdee05aa2266449b20a3c690f299f0d696
mt19aie275 <tiwari.9@iitj.ac.in>
2024-04-04 14:33:26 +0530

    updated tanh

--- tanh.py
+++ tanh.py
@@ -1,6 +1,6 @@
 def tanh(x):
   return np.sinh(x) / np.cosh(x)# Define the range of x-values
-x = np.linspace(-5, 5, 1000)  # 1000 points between -5 and 5# Calculate tanh values
+x = np.linspace(-3.5, 4.6, 100)  # 100 points
 y = tanh(x)# Plot the graph
 plt.plot(x, y)# Add labels and title
 plt.xlabel("x")
